b8854237330522f2e7f87db0e71485867938f13d
uedayusuke <uedayusuke1107@gmail.com>
2020-03-23 21:22:46 +0900

    [Finish]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '71492afcd21be78d39e992aafac24153fe9367042d167a6808edff219b9666c1659966c22a0ad545cbcf317ded002c4861b03b6008b246675348da7407402b2c'
